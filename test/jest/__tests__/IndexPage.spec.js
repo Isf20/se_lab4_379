49179d9cd6aa60f59abacfa6dbd869d3a7afe80b
Isf20 <Ilada.isf.com>
2024-03-10 22:01:28 +0700

    index_function-page'js

--- test/jest/__tests__/IndexPage.spec.js
+++ test/jest/__tests__/IndexPage.spec.js
@@ -7,7 +7,7 @@ describe('IndexPage', () => {
     let header = wrapper.find('.htmlClass h1')
     expect(header.exists()).toBe(true)
     expect(header.text())
-      .toBe('Vue is awesome.')
+      .toBe('Vue is Awesome')
   })
 
   it('check text content to be as defined in variable', () => {
